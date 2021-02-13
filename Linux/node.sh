dd27ec0569ef625e023ce560c3d119964bb373f5
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2021-02-13 01:42:32 -0300

    create script for installing nvm, node 14 and yarn

---
+++ Linux/node.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+echo "
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+" >> ~/.zshrc
+nvm install 14
+npm install --global yarn
