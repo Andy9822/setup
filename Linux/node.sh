0c9806e92817999a118e599166d9415ea4be8829
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2021-02-13 17:16:30 -0300

    fix: reload zsh profile after installing nvm and before using it

--- Linux/node.sh
+++ Linux/node.sh
@@ -5,6 +5,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 " >> ~/.zshrc
-nvm install 14
-npm install --global yarn
+
 source ~/.zshrc
+
+nvm install 14
+
+npm install --global yarn
