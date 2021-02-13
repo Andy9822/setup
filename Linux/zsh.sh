5a5b8f612327f784c68b53184c2b63ce32dcef12
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2021-02-13 16:59:01 -0300

    Create oh-my-zsh install script

---
+++ Linux/zsh.sh
@@ -0,0 +1,3 @@
+yes Y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh)
+zsh
--- linux.sh
+++ linux.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+
+# Install zsh before running any other script
+echo "\e[32mInstalling zsh \e[39m" && "./Linux/zsh.sh"
+
 for file in ./Linux/*; do
     tool=$(basename "$file" | cut -d. -f1)
-    echo "\e[32mInstalling $tool \e[39m"
-    [ -f "$file" ] && [ -x "$file" ] && "$file"
+    if [ -f "$file" ] && [ -x "$file" ] && [ $(basename "$file" | cut -d. -f1) != "zsh" ]
+    then 
+        echo "\e[32mInstalling $tool \e[39m"
+        echo "$file"
+    fi
 done
