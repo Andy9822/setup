f1f96a87429733f9d2993d7303fd4fb232705c23
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2021-02-13 17:07:34 -0300

    fix: use sudo to set zsh as default terminal

--- Linux/zsh.sh
+++ Linux/zsh.sh
@@ -1,4 +1,4 @@
 sudo apt install zsh -y
 yes Y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
 zsh
