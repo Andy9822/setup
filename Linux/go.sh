d867c3c93b6a1d1e2ce422096b825a29c9bad685
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2021-03-18 01:20:52 -0300

    Fix Golang PATH string generation in .zshrc

--- Linux/go.sh
+++ Linux/go.sh
@@ -6,7 +6,7 @@ sudo tar -C /usr/local -xzf $go_version.linux-amd64.tar.gz
 
 echo "
 # go
-export PATH=$PATH:/usr/local/go/bin
-export GOPATH=$HOME/go
-export PATH="$GOPATH/bin:$PATH"
+export PATH=\$PATH:/usr/local/go/bin
+export GOPATH=\$HOME/go
+export PATH="\$GOPATH/bin:\$PATH"
 " >> ~/.zshrc
